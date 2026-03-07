8053c2affd79e147fd8f3e70dd9a55dc74d01896
Bradley <bradconn3932@gmail.com>
2026-03-06 19:53:45 -0500

    body photo and background added

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
 // Becoming a Wizard begins here! 
-const h2 = document.createElement("h2");
-
-h2.textContent = "This content added by JavaScript";
-
-document.querySelector("body").appendChild(h2);
